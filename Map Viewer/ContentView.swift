61629c20acb439cf17e458c991f81aa38bb5a6fa
nooglersoon <nooglersoon@outlook.co.id>
2023-12-05 16:24:55 +0700

    Initial Commit

---
+++ Map Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map Viewer
+//
+//  Created by fauzia076860 on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map Viewer/Map_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_ViewerApp.swift
+//  Map Viewer
+//
+//  Created by fauzia076860 on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
