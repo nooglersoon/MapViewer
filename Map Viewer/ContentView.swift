be174d07d9c6c0bc0174146b54be73e924ae8d5c
nooglersoon <nooglersoon@outlook.co.id>
2023-12-06 11:03:44 +0700

    Handle import geojson

--- Map Viewer/ContentView.swift
+++ Map Viewer/ContentView.swift
@@ -1,18 +1,19 @@
 import SwiftUI
+import CodableGeoJSON
 import MapKit
 
 struct ContentView: View {
     @State private var isImporting: Bool = false
     @State private var region = MKCoordinateRegion(
-            // Set your desired location
-            center: CLLocationCoordinate2D(
-                latitude: 51.507222,
-                longitude: -0.1275),
-            // Give location span from the center
-            span: MKCoordinateSpan(
-                latitudeDelta: 0.5,
-                longitudeDelta: 0.5)
-        )
+        // Set your desired location
+        center: CLLocationCoordinate2D(
+            latitude: 51.507222,
+            longitude: -0.1275),
+        // Give location span from the center
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5)
+    )
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             Map(coordinateRegion: $region)
@@ -51,14 +52,57 @@ struct ContentView: View {
                 else { return }
                 guard let restoredData = try? Data(contentsOf: selectedFile) else {
                     return
-                  }
-                print("DBG \(restoredData)")
+                }
+                do {
+                    switch try JSONDecoder().decode(GeoJSON.self, from: restoredData) {
+                    case .feature(let feature, _):
+                        handleGeometry(feature.geometry)
+                    case .featureCollection(let featureCollection, _):
+                        for feature in featureCollection.features {
+                            handleGeometry(feature.geometry)
+                        }
+                    case .geometry(let geometry, _):
+                        handleGeometry(geometry)
+                    }
+                } catch {
+                    // Handle decoding error
+                }
             } catch (let error) {
                 print(error)
                 // Handle failure.
             }
         }
     }
+
+    func handleGeometry(_ geometry: GeoJSON.Geometry?) {
+        guard let geometry = geometry else { return }
+
+        switch geometry {
+        case .point(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .multiPoint(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .lineString(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .multiLineString(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .polygon(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .multiPolygon(let coordinates):
+            print("DBG \(coordinates)")
+            break
+        case .geometryCollection(let geometries):
+            for geometry in geometries {
+                handleGeometry(geometry)
+            }
+        }
+    }
+    
 }
 
 #Preview {
--- Map Viewer/EmptyProperties.swift
+++ Map Viewer/EmptyProperties.swift
@@ -1,8 +1,3 @@
-//
-//  EmptyProperties.swift
-//  Map Viewer
-//
-//  Created by fauzia076860 on 06/12/23.
-//
-
 import Foundation
+
+struct EmptyProperties: Codable {}
